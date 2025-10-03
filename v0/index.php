401dfec327287106c375b6069b1f7ecd20f88de7
drweb_000 <drweb_000@192.168.0.105>
2025-10-03 11:41:53 +0300

    Избавился от левого null, который выводился после корректного json

--- v0/index.php
+++ v0/index.php
@@ -13,9 +13,7 @@ $method_handler_class = 'losthost\\OberdeskAPI\\input\\Input'. $method;
 
 if (is_a($method_handler_class, AbstractInput::class, true)) {
     $method_handler = new $method_handler_class();
-    $result = $method_handler->process();
-    echo json_encode($result);
+    $method_handler->process();
 } else {
     throw new \Exception('Не верный метод '. $method);
 }
-
